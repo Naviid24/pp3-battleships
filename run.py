24125b31bd483cad48112539a3298b216a85c26f
Navid bahadorani <naviid1989@gmail.com>
2024-09-03 15:23:07 +0000

    Create ship class

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+class Ship:
+    """
+    The main ship object returns the size of the ship
+    and any ships that have been sunk.
+    """
+    def __init__(self, size):
+        self.size = size
+        self.positions = []
+
+    def is_sunk(self):
+        return len(self.positions) == 0
